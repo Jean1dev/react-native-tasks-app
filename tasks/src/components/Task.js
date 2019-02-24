b3907cfb1e5d4dc21438a0b3cb2dfa4fd4d6a94b
Jean <jeanlucafp@gmail.com>
2019-02-23 23:50:30 -0300

    finalizado front- offline
    ..

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-import { View, StyleSheet, Text } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TouchableWithoutFeedback,
+    TouchableOpacity
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import moment from 'moment'
 import 'moment/locale/pt-br'
-
+import Swipeable from 'react-native-swipeable'
 import _styles from '../style';
 
 export default props => {
@@ -23,18 +29,40 @@ export default props => {
 
     const descStyle = props.doneAt !== null ? { textDecorationLine: 'line-through' } : {}
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.checkContainer}>{check}</View>
-            <View>
-                <Text style={[styles.description, descStyle]}>
-                    {props.desc}
-                </Text>
-                <Text style={styles.date}>
-                    {moment(props.estimateAt).locale('pt-br').format('ddd, D [de] MMMM [de] YYYY')}
-                </Text>
-            </View>
+    const leftContent = (
+        <View style={styles.exclude}>
+            <Icon name='trash' size={20} color='#FFF' />
+            <Text style={styles.excludeText}>Excluir</Text>
         </View>
+    )
+
+    const rightContent = [
+        <TouchableOpacity
+            style={[styles.exclude, { justifyContent: 'flex-start', paddingLeft: 20 }]}
+            onPress={() => props.onDelete(props.id)}>
+            <Icon name='trash' size={30} color='#FFF' />
+        </TouchableOpacity>,
+    ]
+
+    return (
+        <Swipeable leftActivationDistance={200}
+            onLeftActionActivate={() => props.onDelete(props.id)}
+            leftContent={leftContent}
+            rightButtons={rightContent}>
+            <View style={styles.container}>
+                <TouchableWithoutFeedback onPress={() => props.onToggleTask(props.id)}>
+                    <View style={styles.checkContainer}>{check}</View>
+                </TouchableWithoutFeedback>
+                <View>
+                    <Text style={[styles.description, descStyle]}>
+                        {props.desc}
+                    </Text>
+                    <Text style={styles.date}>
+                        {moment(props.estimateAt).locale('pt-br').format('ddd, D [de] MMMM [de] YYYY')}
+                    </Text>
+                </View>
+            </View>
+        </Swipeable>
     )
 }
 
--- tasks/src/screens/Agenda.js
+++ tasks/src/screens/Agenda.js
@@ -1,27 +1,124 @@
 import React, { Component } from 'react';
 
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    FlatList,
+    TouchableOpacity,
+    Platform,
+    AsyncStorage
+} from 'react-native';
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../style'
 import Task from '../components/Task'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import AddTask from './AddTask'
+import ActionButton from 'react-native-action-button'
 // import styles from './styles';
 
 export default class Agenda extends Component {
+
+    state = {
+        tasks: [],
+        visibleTasks: [],
+        showTaskDone: true,
+        showAddTask: false
+    }
+
+    deleteTask = async id => {
+        return
+        try {
+            await axios.delete(`${server}/tasks/${id}`)
+            await this.loadTasks()
+        } catch (err) {
+            showError(err)
+        }
+    }
+
+    addTask = async task => {
+        return
+        try {
+            await axios.post(`${server}/tasks`, {
+                desc: task.desc,
+                estimateAt: task.date
+            })
+
+            this.setState({ showAddTask: false }, this.loadTasks)
+        } catch (err) {
+            showError(err)
+        }
+    }
+
+    filterTask = () => {
+        let visibleTasks = null
+        if (this.state.showTaskDone) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+        this.setState({ visibleTasks })
+        AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    }
+
+    toggleFilter = () => {
+        this.setState({ showTaskDone: !this.state.showTaskDone }, this.filterTask())
+    }
+
+    toggleTask = id => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if (task.id === id) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+        this.setState({ tasks }, this.filterTask())
+    }
+
+    componentDidMount = async () => {
+        const data = await AsyncStorage.getItem('tasks')
+        const tasks = JSON.parse(data) || []
+        this.setState({ tasks }, this.filterTask())
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <ImageBackground source={todayImage} style={styles.background}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onSave={this.addTask}
+                    onCancel={() => this.setState({ showAddTask: false })} />
+                <ImageBackground source={image}
+                    style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                            <Icon name='bars' size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.toggleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
-                        <Text style={styles.title}>Hoje</Text>
-                        <Text style={styles.subtitle}> {moment().locale('pt-br').format('ddd, D [de] MMMM')} </Text>
+                        <Text style={styles.title}>{this.props.title}</Text>
+                        <Text style={styles.subtitle}>
+                            {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+                        </Text>
                     </View>
                 </ImageBackground>
-                <View style={styles.taskContainer}>
-                    <Task desc='Tarefa pendente' estimateAt={new Date()}></Task>
+                <View style={styles.taksContainer}>
+                    <FlatList data={this.state.visibleTasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({ item }) =>
+                            <Task {...item} onToggleTask={this.toggleTask}
+                                onDelete={this.deleteTask} />} />
                 </View>
-            </View>
+                <ActionButton buttonColor={styleColor}
+                    onPress={() => { this.setState({ showAddTask: true }) }} />
+            </View >
         )
     }
 }
@@ -54,6 +151,12 @@ const styles = StyleSheet.create({
     },
     taskContainer: {
         flex: 7,
+    },
+    iconbar: {
+        marginTop: Platform.OS === 'ios' ? 30 : 10,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     }
 });
 
