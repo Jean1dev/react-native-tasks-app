d450716080fefb387a4c1335f8dc5ae9f6282609
Jean <jeanlucafp@gmail.com>
2019-03-23 14:36:36 -0300

    corrigido uma penca de problema
    ..

--- backend/migrations/20180830152747_create_table_tasks.js
+++ backend/migrations/20180830152747_create_table_tasks.js
@@ -1,13 +1,15 @@
 
 exports.up = function (knex, Promise) {
-    return knex.schema.createTable('users', table => {
+    return knex.schema.createTable('tasks', table => {
         table.increments('id').primary()
-        table.string('name').notNull()
-        table.string('email').notNull().unique()
-        table.string('password').notNull()
+        table.string('desc').notNull()
+        table.datetime('estimateAt')
+        table.datetime('doneAt')
+        table.integer('userId').references('id')
+            .inTable('users').notNull()
     })
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTable('users')
+    return knex.schema.dropTable('tasks')
 };
